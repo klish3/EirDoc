05145575d05c0ccb24ca56205456c847f460d685
Tawanda <Kliation@Tawandas-MacBook-Pro.local>
2018-05-12 18:00:20 +0200

    schedule component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 import { ROUTING } from './app.routing';
 import { environment } from '../environments/environment';
 
@@ -16,6 +16,9 @@ import { ScheduleComponent } from './components/schedule/schedule.component';
 import { LibrarySideNavComponent } from './components/library/library-side-nav/library-side-nav.component';
 import { PackageCardComponent } from './shared/package-card/package-card.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule } from 'angular-calendar';
+import { DemoUtilsModule } from './shared/calendar-utils/module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,7 +33,11 @@ import { PackageCardComponent } from './shared/package-card/package-card.compone
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    ROUTING
+    ROUTING,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot(),
+    FormsModule,
+    DemoUtilsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/top-nav/top-nav.component.ts
+++ src/app/shared/top-nav/top-nav.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top-nav.component.css']
 })
 export class TopNavComponent implements OnInit {
-
+  user: string = 'Tawanda';
   constructor() { }
 
   ngOnInit() {
